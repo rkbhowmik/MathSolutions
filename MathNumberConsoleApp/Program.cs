5a263a97951726df0f2990cce2b56eb159200dcf
rajib.bhowmik <rajib.bhowmik@gmail.com>
2017-11-07 11:44:36 +0100

    Add project files.

---
+++ MathNumberConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathNumberConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int sum = 0;
+			Console.Write("Numbers between 1 and 50, divisible by 3 : \n");
+			for (int i = 1; i <= 50; i++)
+			{
+				if (i % 3 == 0)
+				{
+					Console.Write("{0}  ", i);
+					sum += i;
+				}
+			}
+
+			Console.Write("\n\nThe sum : {0} \n", sum);
+			Console.ReadLine();
+		}
+	}
+}
